3d9a11dd61e6022beb561c36854d5a2d356aa58e
Li Yong <kaven276@vip.sina.com>
2015-12-16 17:16:07 +0800

    add ; to index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var fs = require('fs')
   ;
 
 exports.rsParse = require('./lib/RSParser').rsParse;
-exports.fileRSParser = fs.readFileSync(path.join(__dirname, prefix, 'RSParser.js'))
-exports.fileNGResultsetsConverter = fs.readFileSync(path.join(__dirname, prefix, 'ng-resultsets-converter.js'))
-exports.fileJQResultsetsConverter = fs.readFileSync(path.join(__dirname, prefix, 'jq-resultsets-converter.js'))
+exports.fileRSParser = fs.readFileSync(path.join(__dirname, prefix, 'RSParser.js'));
+exports.fileNGResultsetsConverter = fs.readFileSync(path.join(__dirname, prefix, 'ng-resultsets-converter.js'));
+exports.fileJQResultsetsConverter = fs.readFileSync(path.join(__dirname, prefix, 'jq-resultsets-converter.js'));
