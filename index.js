065e35c0ad3c92246359bc4c940fef14f7fd7a12
Li Yong <kaven276@vip.sina.com>
2015-12-16 16:39:13 +0800

    export file text also using index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var fs = require('fs')
+  , path = require('path')
+  , prefix = 'lib'
+  ;
+
+exports.rsParse = require('./lib/RSParser').rsParse;
+exports.fileRSParser = fs.readFileSync(path.join(__dirname, prefix, 'RSParser.js'))
+exports.fileNGResultsetsConverter = fs.readFileSync(path.join(__dirname, prefix, 'ng-resultsets-converter.js'))
+exports.fileJQResultsetsConverter = fs.readFileSync(path.join(__dirname, prefix, 'jq-resultsets-converter.js'))
