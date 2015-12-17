1de3a74cf01a13c7ef77bf3513b3d7aa43c090f6
Li Yong <kaven276@vip.sina.com>
2015-12-17 14:05:50 +0800

    exports.serveFile(req,fname)

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+exports.rsParse = require('./lib/RSParser').rsParse;
+
 var fs = require('fs')
   , path = require('path')
   , prefix = 'lib'
+  , fileCache = {}
   ;
 
-exports.rsParse = require('./lib/RSParser').rsParse;
-exports.fileRSParser = fs.readFileSync(path.join(__dirname, prefix, 'RSParser.js'));
-exports.fileNGResultsetsConverter = fs.readFileSync(path.join(__dirname, prefix, 'ng-resultsets-converter.js'));
-exports.fileJQResultsetsConverter = fs.readFileSync(path.join(__dirname, prefix, 'jq-resultsets-converter.js'));
+exports.serveFile = function(res, fname){
+  var content = fileCache[fname];
+  if (!content) {
+    try {
+      content = fileCache[path] = fs.readFileSync(path.join(__dirname, prefix, fname));
+    } catch (e) {
+      res.setHead(404);
+      res.end();
+      return true;
+    }
+  }
+  res.writeHead(200, {
+    'Content-Length' : content.length.toString(),
+    'Content-Type' : 'application/javascript',
+    'Cache-Control' : 'public, max-age=86400'
+  });
+  res.end(content);
+  return true;
+};
